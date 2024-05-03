26ae17f39e2f0a73d1dc3ca32fe73cae56bc964d
Sss <125960911+SimonaNi@users.noreply.github.com>
2024-05-03 16:37:42 +0300

    added LevelManager (not finished yet)

---
+++ core/src/levels/LevelManager.java
@@ -0,0 +1,27 @@
+package levels;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public class LevelManager {
+    private Game game;
+    private Texture levelSprite;
+
+    public LevelManager(Game game){
+        this.game = game;
+        levelSprite = LoadSave.getTexture(LoadSave.LEVEL_ATLAS);
+        if (levelSprite == null) {
+            // Handle failed loading
+            throw new GdxRuntimeException("Failed to load level atlas.");
+        }
+    }
+
+    public void draw(SpriteBatch batch){
+        batch.draw(levelSprite, 0, 0);
+    }
+
+    public void update(){
+        // Update logic for the level
+    }
+}
